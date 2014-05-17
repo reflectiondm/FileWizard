c557725bb6239ddb2df81af31746762242c2f20e
Andrei <reflection.dm@gmail.com>
2014-05-17 10:03:11 +0400

    Introduce file selector view and bind it to the corresponding viewmodel.

---
+++ FileWizard.Gui/FolderSelector/FolderSelectorViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWizard.Gui.FolderSelector
+{
+    public class FolderSelectorViewModel : ViewModelBase
+    {
+        public FolderSelectorViewModel()
+        {
+            FolderPath = "This is FolderPath. Binding works";
+        }
+
+        private string _folderPath;
+        public string FolderPath
+        {
+            get
+            {
+                return _folderPath;
+            }
+            set
+            {
+                _folderPath = value;
+                RaiseOnPropertyChanged("FolderPath");
+            }
+        }
+    }
+}
