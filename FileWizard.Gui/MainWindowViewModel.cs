c9c96761c0746dbc7de4fe617e3b5f0521bbc026
Andrei <reflection.dm@gmail.com>
2014-05-17 09:45:26 +0400

    Add initial projects, base classes and tests. Make mainwindow show folderSelector template on startup.

---
+++ FileWizard.Gui/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWizard.Gui
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel(IViewModel folderSelectorVM)
+        {
+            ActiveViewModel = folderSelectorVM;
+        }
+
+        public MainWindowViewModel()
+            : this(new FolderSelectorViewModel())
+        {
+
+        }
+
+        private IViewModel _activeViewModel;
+        public IViewModel ActiveViewModel
+        {
+            get
+            {
+                return _activeViewModel;
+            }
+            set
+            {
+                if (_activeViewModel == value)
+                    return;
+
+                _activeViewModel = value;
+                RaiseOnPropertyChanged("ActiveViewModel");
+            }
+        }
+    }
+}
---
+++ FileWizard.Gui/ViewModelBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWizard.Gui
+{
+    /// <summary>
+    /// Base-class implementing essential ViewModel functionality
+    /// </summary>
+    public abstract class ViewModelBase : IViewModel
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        protected void RaiseOnPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
